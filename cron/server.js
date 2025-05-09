340adb5f95947959457a29ab6e93ed2b350ee238
hussaintaha <hussaintaha500@gmail.com>
2025-05-09 07:42:27 +0000

    updated cron port variable

--- cron/server.js
+++ cron/server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const EXTERNAL_API_URL = process.env.EXTERNAL_API_URL;
 
